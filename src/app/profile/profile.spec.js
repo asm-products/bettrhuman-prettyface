fa9624b2a5562b03204cb87f3041950142da7c37
tshields <troy.leland.shields@gmail.com>
2014-10-05 18:49:50 -0600

    Added profile, read review directive, and read comment directive.

---
+++ src/app/profile/profile.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Tests sit right alongside the file they are testing, which is more intuitive
+ * and portable than separating `src` and `test` directories. Additionally, the
+ * build process will exclude all `.spec.js` files from the build
+ * automatically.
+ */
+describe( 'review section', function() {
+  beforeEach( module( 'bettrhuman.review' ) );
+
+  it( 'should have a dummy test', inject( function() {
+    expect( true ).toBeTruthy();
+  }));
+});
+
